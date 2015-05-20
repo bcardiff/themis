a97ff0a12ecbee5a65e5a78d6e6618e8d427e58a
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-05-20 18:20:46 -0300

    update gemfile fix welcome#index

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -2,7 +2,7 @@ class Admin::WelcomeController < Admin::BaseController
   def index
     CourseLog.fill_missings
 
-    start_date = Date.parse(params['start_date']) || Date.today
+    start_date = Date.parse(params['start_date']) rescue Date.today
     @course_logs = CourseLog.where(date: start_date.beginning_of_month..start_date.end_of_month).all.group_by(&:date)
   end
 end
