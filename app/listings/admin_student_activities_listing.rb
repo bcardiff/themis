86f843dcf88c85fdfd429cd3eb9809ef4a81c7f5
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-08-31 03:42:06 -0300

    fix #49. sort student activities

--- app/listings/admin_student_activities_listing.rb
+++ app/listings/admin_student_activities_listing.rb
@@ -1,6 +1,6 @@
 class AdminStudentActivitiesListing < Listings::Base
   model {
-    Student.find(params[:id]).activity_logs
+    Student.find(params[:id]).activity_logs.order(date: :desc)
   }
 
   column :date
