1ba78bdfd35143be4af4f987a36e8f84b0fa4b99
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2015-11-24 02:07:18 -0300

    Added a first draft of the day's courses in dashboard

--- app/controllers/cashier/dashboard_controller.rb
+++ app/controllers/cashier/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Cashier::DashboardController < Cashier::BaseController
   def index
+    courses = Course.ongoing.where(weekday: Date.today.wday)
+    @courses_by_time = {}
+    courses.each do |course|
+      @courses_by_time[course.start_time] ||= []
+      @courses_by_time[course.start_time] << course
+    end
+    @courses_by_time = @courses_by_time.sort
   end
+
 end
