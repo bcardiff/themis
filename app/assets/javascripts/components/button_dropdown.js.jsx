c2817d2a5fbd201c9c402fbf0a07f85eba807d40
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-12-02 20:38:12 -0300

    bigger pack payment buttons

--- app/assets/javascripts/components/button_dropdown.js.jsx
+++ app/assets/javascripts/components/button_dropdown.js.jsx
@@ -2,10 +2,10 @@ var ButtonDropdown = React.createClass({
   render: function() {
     return (
       <div className="btn-group">
-        <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
+        <button type="button" className="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown">
           {this.props.title} <span className="caret"></span>
         </button>
-        <ul className="dropdown-menu">
+        <ul className="dropdown-menu dropdown-menu-lg">
           {this.props.children}
         </ul>
       </div>
