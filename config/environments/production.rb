b5fce29ac952d4183d70f4d13f6a2b63e2a13c87
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-06-26 15:20:35 -0300

    change mailer to use smtp configured from settings in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Settings.smtp_settings.to_h
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@swingcity.com.ar'}
