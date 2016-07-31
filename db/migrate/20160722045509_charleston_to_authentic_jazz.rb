a37679f2e4ddf4dd2dade3aac9df05027c3ebf19
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-07-31 20:32:32 -0300

    fix migration

--- db/migrate/20160722045509_charleston_to_authentic_jazz.rb
+++ db/migrate/20160722045509_charleston_to_authentic_jazz.rb
@@ -1,19 +1,19 @@
 class CharlestonToAuthenticJazz < ActiveRecord::Migration
   def up
-    execute "UPDATE Tracks SET Code = 'AJ_PRIN' WHERE Code = 'CH_PRIN';"
-    execute "UPDATE Tracks SET Code = 'AJ_AVAN' WHERE Code = 'CH_AVAN';"
-    execute "UPDATE Courses SET Code = 'AJ_PRIN_LUN' WHERE Code = 'CH_PRIN_LUN';"
-    execute "UPDATE Courses SET Code = 'AJ_PRIN_MIE' WHERE Code = 'CH_PRIN_MIE';"
-    execute "UPDATE Courses SET Code = 'AJ_AVAN_MIE' WHERE Code = 'CH_AVAN_MIE';"
-    execute "UPDATE Courses SET Code = 'AJ_AVAN_SAB' WHERE Code = 'CH_AVAN_SAB';"
+    execute "UPDATE tracks SET Code = 'AJ_PRIN' WHERE Code = 'CH_PRIN';"
+    execute "UPDATE tracks SET Code = 'AJ_AVAN' WHERE Code = 'CH_AVAN';"
+    execute "UPDATE courses SET Code = 'AJ_PRIN_LUN' WHERE Code = 'CH_PRIN_LUN';"
+    execute "UPDATE courses SET Code = 'AJ_PRIN_MIE' WHERE Code = 'CH_PRIN_MIE';"
+    execute "UPDATE courses SET Code = 'AJ_AVAN_MIE' WHERE Code = 'CH_AVAN_MIE';"
+    execute "UPDATE courses SET Code = 'AJ_AVAN_SAB' WHERE Code = 'CH_AVAN_SAB';"
   end
 
   def down
-    execute "UPDATE Tracks SET Code = 'CH_PRIN' WHERE Code = 'AJ_PRIN';"
-    execute "UPDATE Tracks SET Code = 'CH_AVAN' WHERE Code = 'AJ_AVAN';"
-    execute "UPDATE Courses SET Code = 'CH_PRIN_LUN' WHERE Code = 'AJ_PRIN_LUN';"
-    execute "UPDATE Courses SET Code = 'CH_PRIN_MIE' WHERE Code = 'AJ_PRIN_MIE';"
-    execute "UPDATE Courses SET Code = 'CH_AVAN_MIE' WHERE Code = 'AJ_AVAN_MIE';"
-    execute "UPDATE Courses SET Code = 'CH_AVAN_SAB' WHERE Code = 'AJ_AVAN_SAB';"
+    execute "UPDATE tracks SET Code = 'CH_PRIN' WHERE Code = 'AJ_PRIN';"
+    execute "UPDATE tracks SET Code = 'CH_AVAN' WHERE Code = 'AJ_AVAN';"
+    execute "UPDATE courses SET Code = 'CH_PRIN_LUN' WHERE Code = 'AJ_PRIN_LUN';"
+    execute "UPDATE courses SET Code = 'CH_PRIN_MIE' WHERE Code = 'AJ_PRIN_MIE';"
+    execute "UPDATE courses SET Code = 'CH_AVAN_MIE' WHERE Code = 'AJ_AVAN_MIE';"
+    execute "UPDATE courses SET Code = 'CH_AVAN_SAB' WHERE Code = 'AJ_AVAN_SAB';"
   end
 end
