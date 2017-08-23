12da4e48f3ec6b708e0f9429705981874bc3fb97
Brian J. Cardiff <bcardiff@manas.tech>
2017-08-23 01:23:27 -0300

    update horarios tap miércoles

--- db/seeds.rb
+++ db/seeds.rb
@@ -142,6 +142,7 @@ course "TP_PRIN_VIE", name: "Tap - Principiantes - Viernes", weekday: 5, valid_s
 course "TP_INT1_LUN", name: "Tap - Intermedios 1 - Lunes mañana", weekday: 1, track: track("TP_INT1"), place: swing_city, valid_since: Date.new(2017,6,1), valid_until: nil, start_time: '11:00'
 course "TP_INT1_MAR", name: "Tap - Intermedios 1 - Martes", weekday: 2, track: track("TP_INT1"), place: swing_city, valid_until: Date.new(2016,12,31), start_time: '18:00'
 course "TP_INT1_MAR2", name: "Tap - Intermedios 1 - Martes", weekday: 2, valid_since: Date.new(2017,1,1), track: track("TP_INT1"), place: swing_city, start_time: '19:00'
+course "TP_INT1_MIE", name: "Tap - Intermedios 1 - Miércoles", weekday: 3, track: track("TP_INT1"), place: swing_city, valid_since: Date.new(2017,8,23), start_time: '11:00'
 course "TP_INT1_VIE", name: "Tap - Intermedios 1 - Viernes", weekday: 5, valid_since: Date.new(2015,6,1), track: track("TP_INT1"), place: swing_city, start_time: '20:00'
 
 course "TP_INT2_MAR", name: "Tap - Intermedios 2 - Martes", weekday: 2, track: track("TP_INT2"), place: swing_city, start_time: '18:00'
