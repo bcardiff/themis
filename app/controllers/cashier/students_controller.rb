69eb0a6acaa158e6136c5f4250ffa6d7426bb0ae
Brian J. Cardiff <bcardiff@gmail.com>
2018-04-03 19:18:18 -0300

    Turn off welcome email

--- app/controllers/cashier/students_controller.rb
+++ app/controllers/cashier/students_controller.rb
@@ -38,11 +38,11 @@ class Cashier::StudentsController < Cashier::BaseController
         TeacherCashIncomes::NewCardIncome.create_cashier_card_payment!(current_user.teacher, student, School.today)
       end
 
-      begin
-        StudentNotifications.welcome(student).deliver_now if student.email.present?
-      rescue => ex
-        logger.warn ex
-      end
+      # begin
+      #   StudentNotifications.welcome(student).deliver_now if student.email.present?
+      # rescue => ex
+      #   logger.warn ex
+      # end
 
       render json: {status: :ok, student: student_json(student)}
     else
