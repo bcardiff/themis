26986317b487623fc48f7a4385747d65340198ae
Brian J. Cardiff <bcardiff@gmail.com>
2019-03-05 00:46:13 -0300

    Refactor: remove name_with_track_as_context

--- app/models/course.rb
+++ app/models/course.rb
@@ -45,10 +45,6 @@ class Course < ActiveRecord::Base
     "#{self.start_time.hour}hs"
   end
 
-  def name_with_track_as_context
-    code.sub("#{track.code}_", "")
-  end
-
   def css_prefix
     "#{track.css_prefix} #{hashtag.try &:underscore}"
   end
