7462ea98475b72533366f07f0f5fa9db0e237448
Brian J. Cardiff <bcardiff@gmail.com>
2020-01-29 09:50:13 -0300

    fix button dropdown alignment on mobile

--- app/assets/javascripts/components/button_dropdown.js.jsx
+++ app/assets/javascripts/components/button_dropdown.js.jsx
@@ -1,11 +1,11 @@
 var ButtonDropdown = React.createClass({
-  render: function() {
+  render: function () {
     return (
       <div className="btn-group">
         <button type="button" className="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown">
           {this.props.title} <span className="caret"></span>
         </button>
-        <ul className="dropdown-menu dropdown-menu-right dropdown-menu-lg">
+        <ul className="dropdown-menu sm-dropdown-menu-right dropdown-menu-lg">
           {this.props.children}
         </ul>
       </div>
