40b74284e614998903574dc06a167b577c6ab8ca
Brian J. Cardiff <bcardiff@gmail.com>
2022-03-29 15:22:41 -0300

    Build docker images using GitHub Actions

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 # This will load the script from this repository. Make sure to point to a specific commit so the build continues to work
 # event if breaking changes are introduced in this repository
-source <(curl -s https://raw.githubusercontent.com/manastech/ci-docker-builder/06a82a6f0719c0c64b9e2dbde7045d574e454d01/build.sh)
+source /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/manastech/ci-docker-builder/6bcc63f7176e6eb60385d027469bd261952d914c/build.sh)"
 
 # Prepare the build
 dockerSetup
